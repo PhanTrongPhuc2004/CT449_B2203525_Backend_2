59c666bcf189239c9e7d81cafa7eab303d1fc6da
PhanTrongPhuc <phantrongphuc12004@gmail.com>
2025-02-25 22:41:44 +0700

    Cai dat cac route, controller va xu ly loi

--- app.js
+++ app.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRoute = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Default route
 app.get("/", (req, res) => {
-    res.json({message: "Welcome to contact book application."});
+    res.json({ message: "Welcome to contact book application." });
 });
 
+// route example for error handling
+app.get("/error", (req, res, next) => {
+    const err = new Error;
+    return next(err);
+});
+
+// Routes
+app.use("/api/contacts", contactsRoute);
+
+// handle 404 response
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not Found"));
+});
+
+// handle all errors
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 module.exports = app;
